58e5fa51c973e46010e0df53f6a405e159d25c0e
Yin <hsieh.y.chen@uit.no>
2023-03-28 14:04:48 +0200

    added remaining day 1 exercises

---
+++ basics_day1/greeter.py
@@ -0,0 +1,10 @@
+"""
+greeter.py
+
+Friendly greeting program:
+
+    This program says hello, asks for your name,
+    and prints a greeting with the given name
+
+Use the input function, print function, and f-strings.
+"""
---
+++ basics_day1/replace_in_str.py
@@ -0,0 +1,9 @@
+"""
+replace_in_str.py
+
+Replace Within a String
+
+This script takes in a string with words separated by commas,
+and replaces the commas with spaces.
+Then it prints out the resulting string.
+"""
