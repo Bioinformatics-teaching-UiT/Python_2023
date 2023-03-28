dafd85b7b3e024963bef17f0690906cd5593c032
Yin <hsieh.y.chen@uit.no>
2023-03-28 14:07:58 +0200

    added remaining presentations

--- basics_day1/pizza_area.py
+++ basics_day1/pizza_area.py
@@ -1,5 +1,7 @@
 """
-Day 1: Pizza Calculator Script
+pizza_area.py
+
+A Pizza Calculator Script
 
 Write a script that calculates the total volume of a pizza of radius z and height a.
 Print out this volume in an informative way.
