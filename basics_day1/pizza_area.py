512269c0a2f9fedb4bc31a5f19202b9826bdfcf0
Yin <hsieh.y.chen@uit.no>
2023-03-28 13:35:37 +0200

    added pizza exercise

---
+++ basics_day1/pizza_area.py
@@ -0,0 +1,10 @@
+"""
+Day 1: Pizza Calculator Script
+
+Write a script that calculates the total volume of a pizza of radius z and height a.
+Print out this volume in an informative way.
+
+Use variables for all numbers.
+
+Hint: you can approximate pi with 22/7
+"""
