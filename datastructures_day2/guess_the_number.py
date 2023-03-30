b4efe398c21bb24c81b220fcc22a0e2055f674f9
Yin <hsieh.y.chen@uit.no>
2023-03-30 13:30:58 +0200

    add remaining day 2 exercises

---
+++ datastructures_day2/guess_the_number.py
@@ -0,0 +1,9 @@
+"""
+guess_the_number.py
+
+Let's play a game where you ask the user to input a number,
+see if it matches your own number, and you tell the user if the number is correct or not.
+
+If it is correct, end the game. If not, continue.
+
+"""
