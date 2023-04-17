ab74a6fc11616d8cd0c1afe291ca91fa3477a506
Yin <hsieh.y.chen@uit.no>
2023-04-17 15:10:34 +0200

    updated replace in string

--- basics_day1/replace_in_str.py
+++ basics_day1/replace_in_str.py
@@ -7,3 +7,11 @@ This script takes in a string with words separated by commas,
 and replaces the commas with spaces.
 Then it prints out the resulting string.
 """
+
+# replace(old,new,count), if count isn't given, default to all
+
+somestr = 'words   ,separated,with, comma,'
+
+print(somestr.count(',')) # count instances of commas in somestr
+commareplaced = somestr.replace(',', ' ') # replace spaces with commas
+print(commareplaced)
