bfaba3405c29c539d731eb5dbaeab2b7c8b16256
Yin <hsieh.y.chen@uit.no>
2023-04-17 15:11:07 +0200

    updated greeter script with fullname bonus

--- basics_day1/greeter.py
+++ basics_day1/greeter.py
@@ -7,4 +7,20 @@ Friendly greeting program:
     and prints a greeting with the given name
 
 Use the input function, print function, and f-strings.
+
+BONUS: Give the program your full name and print out your first
+and last name separately! 
 """
+
+# ask the user for their name
+fullname = input('Please type in your full name, separated by a space: ')
+
+namelist = fullname.split()
+print(namelist[0])
+print(namelist[1])
+
+# put the name in an f-string
+message = f'Hello {namelist[0]}, your last name is {namelist[1]}.'
+
+# print out the greeting message
+print(message)
