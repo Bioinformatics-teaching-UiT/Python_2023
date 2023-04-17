a18ed3fc6cd2269b8b59a758decde6d481590a2d
Yin <hsieh.y.chen@uit.no>
2023-04-17 15:12:06 +0200

    update pizza area calculator

--- basics_day1/pizza_area.py
+++ basics_day1/pizza_area.py
@@ -3,10 +3,35 @@ pizza_area.py
 
 A Pizza Calculator Script
 
-Write a script that calculates the total volume of a pizza of radius z and height a.
+Write a script that calculates the total volume 
+of a pizza of radius z and height a.
 Print out this volume in an informative way.
 
 Use variables for all numbers.
 
 Hint: you can approximate pi with 22/7
 """
+
+# here I define my variables
+pi = 22/7
+z = 15
+a = 5
+
+# here I calculate the volume V
+V = pi * z * z * a
+
+# here I print out my volume
+print('My pizza volume is', V)
+
+
+
+
+
+
+
+
+
+
+
+
+
